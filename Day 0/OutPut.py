129607058e88e720250770d899876d1b112e2100
Soundarya0 <53481890+Soundarya0@users.noreply.github.com>
2020-02-07 13:13:50 +0530

    Add files via upload

---
+++ Day 0/OutPut.py
@@ -0,0 +1,8 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:/Users/HP/Downloads/30 Days of Code/Day 0/HelloWorld.py ======
+Welsome to 30 Days of Code!
+Hello, World.
+Welsome to 30 Days of Code!
+>>> 
